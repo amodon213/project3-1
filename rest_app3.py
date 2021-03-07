1e3aee58e7e87c5490d79cfab0b31e08cc8fd30e
ikitaichik <77975337+ikitaichik@users.noreply.github.com>
2021-03-07 19:04:12 +0200

    Update rest_app3.py

--- rest_app3.py
+++ rest_app3.py
@@ -1,1 +1,31 @@
 
+from flask import Flask, request
+from db_connector import *
+import os
+import signal
+
+app = Flask(__name__)
+
+
+# supported methods
+@app.route('/users/<id>')
+def user(id):
+        try:
+            name = get_id(id)
+            if name == '':
+                raise Exception
+            else:
+                return {'status': 'ok', 'user name': name}, 200
+        except:
+            return {'status': 'error', 'reason': "No such id"}, 500  # status code
+
+@app.route('/')
+    return 200
+
+@app.route('/stop_server')
+def stop_server():
+    os.kill(os.getpid(), signal.SIGINT)
+    return 'Server stopped'
+
+
+app.run(host='0.0.0.0')
