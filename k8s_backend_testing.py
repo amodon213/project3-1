bb2641d12f6d7c9371c653a6f7ab2127ad2c35dd
ikitaichik <77975337+ikitaichik@users.noreply.github.com>
2021-03-08 15:09:29 +0200

    Update k8s_backend_testing.py

--- k8s_backend_testing.py
+++ k8s_backend_testing.py
@@ -7,6 +7,9 @@ try:
     res = requests.get(f'{url}/users/1')
 
     print(res.json())
+    
+    res = requests.get('http://0.0.0.0:5000/users/1')
 
+    print(res.json())
 except Exception as e:
     print(e)
