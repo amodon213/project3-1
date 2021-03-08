70121b8d1a420aa16ff8950148ee45e67843c8d9
ikitaichik <77975337+ikitaichik@users.noreply.github.com>
2021-03-08 14:57:34 +0200

    Update rest_app.py

--- rest_app.py
+++ rest_app.py
@@ -8,37 +8,38 @@ app = Flask(__name__)
 
 # supported methods
 @app.route('/users/<id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
-def user(id):
-    if request.method == 'POST':
+def user(sent_id):
+    if request.method == 'GET':
         try:
-            name = request.json.get('name')
-            insert_user(id, name)
-            return {'status': 'ok', 'user added': name}, 200  # status code
-        except:
-            return {'status': 'error', 'reason': "No such id"}, 500 # status code# status cod
-
-    elif request.method == 'GET':
-        try:
-            name = get_id(id)
+            name = get_id(sent_id)
             if name == '':
                 raise Exception
             else:
                 return {'status': 'ok', 'user name': name}, 200
         except:
-            return {'status': 'error', 'reason': "No such id"}, 500  # status code
+            return {'status': 'error', 'reason': "No such id haha}, 500  # status code
 
+    elif request.method == 'POST':
+        try:
+            name = request.json.get('name')
+            insert_user(sent_id, name)
+            return {'status': 'ok', 'user added': name}, 200  # status code
+        except:
+            return {'status': 'error', 'reason': "No such id"}, 500 # status code# status cod
+
+    
     elif request.method == 'PUT':
         try:
             name = request.json.get('name')
-            update_user(name, id)
+            update_user(name, sent_id)
             return {'status': 'ok', 'user_updated': name}, 200  # status code
         except:
             return {"status": "error", "reason": "No such id"}, 500
 
     elif request.method == 'DELETE':
         try:
-            delete_user(id)
-            return {'status': 'ok', 'user_deleted': id}, 200  # status code
+            delete_user(sent_id)
+            return {'status': 'ok', 'user_deleted': sent_id}, 200  # status code
         except:
             return {'status': 'error', 'reason': "No such id"}, 500
 
