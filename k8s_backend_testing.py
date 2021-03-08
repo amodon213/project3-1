7d168b028115b088cd86fa321dff84289e5ea473
ikitaichik <77975337+ikitaichik@users.noreply.github.com>
2021-03-08 14:40:23 +0200

    Update k8s_backend_testing.py

--- k8s_backend_testing.py
+++ k8s_backend_testing.py
@@ -1,12 +1,12 @@
 import requests
 
 try:
-    url = open("k8s_url.txt",'r').read().strip()
+    with open("k8s_url.txt", "r") as f:
+        url = f.read()
+        
     res = requests.get(f'{url}/users/1')
 
     print(res.json())
-    res = requests.get('http://0.0.0.0:5000/users/1')
-    print(res.json())
-            
+
 except Exception as e:
     print(e)
