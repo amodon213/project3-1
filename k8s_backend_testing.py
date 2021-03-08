971fbb193836ed3a059b5dffcf2a7464d3c5deb8
ikitaichik <77975337+ikitaichik@users.noreply.github.com>
2021-03-08 13:28:57 +0200

    Update k8s_backend_testing.py

--- k8s_backend_testing.py
+++ k8s_backend_testing.py
@@ -1,11 +1,13 @@
 import requests
+import time
+
+time.sleep(15)
 
 try:
     url = open("k8s_url.txt",'r').read().strip()
-    url = "\"" + url +  "/users/1" + "\""
-    
+    url +=  "/users/1"   
     print(url)
-    res = requests.get({url})
+    res = requests.get(url)
 
     print(res.json())
 
