8ab1993e79cc664e3821f53834d2f057d9eb951f
ikitaichik <77975337+ikitaichik@users.noreply.github.com>
2021-03-08 13:07:26 +0200

    Update k8s_backend_testing.py

--- k8s_backend_testing.py
+++ k8s_backend_testing.py
@@ -1,9 +1,9 @@
 import requests
 
 try:
-    url = open("k8s_url.txt",'r').read().split('\n') + "/users/1" 
+    url = open("k8s_url.txt",'r').read().split('\n')
     print(url)
-    res = requests.get(f'{url}')
+    res = requests.get(f'{url}/users/1')
 
     print(res.json())
 
